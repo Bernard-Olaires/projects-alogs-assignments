1cb5e06cc9c02a8c4cfbd55d23b48c66e3b58111
Bernard Olaires <bolairesjr@gmail.com>
2023-03-19 19:31:25 -0700

    added string to do 1

---
+++ strings1.js
@@ -0,0 +1,13 @@
+// Strings To Do 1
+// Write functions using the JavaScript language for all challenges. Use only a single JavaScript file for this assignment. 
+// Do not use built-in methods (unless otherwise instructed). (.length is a property, and is allowed.)
+
+// Remove Blanks
+// Create a function that, given a string, returns all of that string’s contents, but without blanks. 
+// Examples:
+// removeBlanks(" Pl ayTha tF u nkyM usi c ") => "PlayThatFunkyMusic"
+// removeBlanks("I can not BELIEVE it's not BUTTER") => "IcannotBELIEVEit'snotBUTTER"
+
+function removeBlanks(string) {
+
+}
