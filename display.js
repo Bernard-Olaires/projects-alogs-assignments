c6d4e551f4a399401d83bac1710610edaade8471
Bernard Olaires <bolairesjr@gmail.com>
2023-03-26 20:17:15 -0700

    display assignment

---
+++ display.js
@@ -0,0 +1,11 @@
+// Display
+// Use classes, attributes, and methods in the JavaScript language for this challenge. Use only a single JavaScript file for this assignment, it may be the same from the Fronts assignment. All examples are done in order, starting with a new instance of the SLL class.
+
+// Create display() that uses a while loop and a runner to return a string containing all list values. Build what you wish console.log(myList) did!
+
+class SLL {
+    // constructor, other methods, removed for brevity
+    display() {
+        // neatly display nodes in my list
+    }
+}
